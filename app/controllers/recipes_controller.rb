24e43763098ae2f22db81785b802bbb80d63f1eb
David Acosta <davidacosta25081@yahoo.com>
2019-08-26 04:58:00 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build()
+    @recipe.ingredients.build(name: 'Vegetariano')
+    @recipe.ingredients.build(name: 'Non Vegetarian')
   end
 
   def create
