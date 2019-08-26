458b2fd62bc73cdeccf962315e7d4f3af37cf99c
David Acosta <davidacosta25081@yahoo.com>
2019-08-26 04:49:12 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,7 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new	    
+    @recipe = Recipe.new
     2.times do |i|
     @recipe.ingredients.build()
     end
