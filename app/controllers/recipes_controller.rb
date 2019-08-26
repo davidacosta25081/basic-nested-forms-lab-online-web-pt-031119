53296a5b66d2502b5d03461e1682d103262fec64
David Acosta <davidacosta25081@yahoo.com>
2019-08-26 04:46:36 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,8 +8,11 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
-  end
+    @recipe = Recipe.new	    @recipe = Recipe.new
+    2.times do |i|
+      @recipe.ingredients.build()
+    end
+  end	  end
 
   def create
     recipe = Recipe.create(recipe_params)
